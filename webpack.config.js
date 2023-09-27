07ac40f014adda51a87c791192d604f20ff675ad
Hans Naert <hans.naert@vives.be>
2023-09-27 22:54:25 +0200

    add webpack-pwa-manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 module.exports = {
   entry: './src/index.js',
@@ -15,5 +16,28 @@ module.exports = {
   }),
   new WorkboxPlugin.InjectManifest({
     swSrc: './src/sw.js',
+  }),
+  new WebpackPwaManifest({
+    name: 'My Progressive Web App',
+    short_name: 'MyPWA',
+    description: 'My awesome Progressive Web App!',
+    background_color: '#ffffff',
+    crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+    publicPath: './',
+    icons: [
+      {
+        src: path.resolve('./src/assets/logo-512.png'),
+        sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+      },
+      {
+        src: path.resolve('./src/assets/maskable-1024.png'),
+        size: '1024x1024' // you can also use the specifications pattern
+      },
+      {
+        src: path.resolve('./src/assets/maskable-1024.png'),
+        size: '1024x1024',
+        purpose: 'maskable'
+      }
+    ]
   })],  
 };
